d226064442318eb1c805fec1de7fc6cbbd11ea7c
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2020-05-01 14:51:15 +1000

    Update comparison_trendy.sh

--- comparison_trendy.sh
+++ comparison_trendy.sh
@@ -4,12 +4,12 @@ for var in GPP GPP_SD; do
         TRENDY_comparison/GOSIF_$var'_'2001-2017.nc
     cdo -L -sellonlatbox,112.25,153.75,-43.75,-10.25 \
         TRENDY_comparison/GOSIF_$var'_'2001-2017.nc \
-        TRENDY_comparison/GOSIF_$var'_'australia.nc       
-    cdo -b F64 -L -divc,1e+15 -yearsum -settaxis,2001-01-01,00:00,1month \
-        -seldate,2001-07-16,2017-06-16 -sellonlatbox,112.25,153.75,-43.75,-10.25 \
-        -mul TRENDY_comparison/GOSIF_$var'_'australia.nc -gridarea \
-        TRENDY_comparison/GOSIF_$var'_'australia.nc \
-        TRENDY_comparison/GOSIF_$var'_'australia_area_weighted.nc       
+        TRENDY_comparison/GOSIF_$var'_'australia.nc
+    cdo -b F64 -L -divc,1e+15 -yearsum -settaxis,2000-01-01,00:00,1month \
+        -seldate,2000-07-15,2017-06-16 -sellonlatbox,112.25,153.75,-43.75,-10.25 \
+        -mul TRENDY_comparison/GOSIF_$var'_'2000-2017.nc -gridarea \
+        TRENDY_comparison/GOSIF_$var'_'2000-2017.nc \
+        TRENDY_comparison/GOSIF_$var'_'australia_area_weighted.nc
     cdo -L -divc,1e+15 -ymonmean -mul \
         TRENDY_comparison/GOSIF_$var'_'australia.nc -gridarea \
         TRENDY_comparison/GOSIF_$var'_'australia.nc \
